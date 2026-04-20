e2d4d5a1aff90d58fbb242c1b49b727652b147fb
safenessinc-cellss <safeness.inc@gmail.com>
2026-04-20 10:38:48 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import SchedulingPage from './pages/SchedulingPage.tsx';
 import ChatWidget from './components/ChatWidget.tsx';
 import './index.css';
+import './i18n'; // 👈 ESTA LÍNEA ES LA QUE FALTA
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,6 @@ createRoot(document.getElementById('root')!).render(
       </Routes>
       <ChatWidget />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
 
